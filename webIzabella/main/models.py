59d18a82e9263c5bf9bbb7147efc82c792319aad
Jasur <mr.jasur09@mail.ru>
2021-06-14 13:41:01 +0500

    lab5 code almost done

--- webIzabella/main/models.py
+++ webIzabella/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Products(models.Model):
+    #id = models.AutoField(primary_key=True)
+    name = models.CharField('Модель', max_length=50, default='')
+    material = models.CharField('Материал', max_length=50, default='')
+    mattress = models.CharField('Матрац', max_length=50, default='')
+    price = models.IntegerField('Цена', default='0')
+
+    def __str__(self):
+        return '%s %s %s %d' %(self.name, self.material, self.mattress, self.price)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
